ded9975fc5590cc6bc56231db1f8ed607f4d2fb0
trt <seifbkh97@gmail.com>
2025-02-26 17:57:41 +0100

    freedom of speach

--- src/main/java/tn/esprit/test/UIFreelance.java
+++ src/main/java/tn/esprit/test/UIFreelance.java
@@ -20,7 +20,7 @@ public class UIFreelance extends Application {
     public void start(Stage primaryStage) throws SQLException {
         Router.initialize(primaryStage);
         Controller con = new Controller();
-        Session.start(new ServiceUtilisateur().getUtilisateurById(1));
+//        Session.start(new ServiceUtilisateur().getUtilisateurById(1));
         con.login();
     }
 }

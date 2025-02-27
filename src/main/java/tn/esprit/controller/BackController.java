5310a220b6ae20cc7a5272420b08b18a9ddde9e3
trt <seifbkh97@gmail.com>
2025-02-27 21:37:24 +0100

    jst lemme commit

--- src/main/java/tn/esprit/controller/BackController.java
+++ src/main/java/tn/esprit/controller/BackController.java
@@ -1,13 +1,34 @@
 package tn.esprit.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.layout.BorderPane;
 
-public class BackController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BackController implements Initializable {
+    @FXML
+    private BorderPane freelancerBorderPane;
+    @FXML
+    private BorderPane evenementBorderPane;
+    @FXML
+    private BorderPane utilisateurBorderPane;
+    @FXML
+    private BorderPane equipeBorderPane;
+    @FXML
+    private BorderPane coursBorderPane;
+    @FXML
+    private BorderPane storeBorderPane;
     @FXML
     private BorderPane borderPane;
 
     public BorderPane getBorderPane(){
         return borderPane;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }

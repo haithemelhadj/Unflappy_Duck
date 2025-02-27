1231edc79255c798bbcd633a4a5c293e305a56db
trt <seifbkh97@gmail.com>
2025-02-27 12:08:52 +0100

    reverse works

--- src/main/java/tn/esprit/controller/MarketService.java
+++ src/main/java/tn/esprit/controller/MarketService.java
@@ -4,10 +4,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
 import tn.esprit.models.Service;
 import tn.esprit.services.ServiceService;
@@ -16,8 +14,10 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 import java.net.URL;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 
 public class MarketService implements Initializable {
@@ -33,10 +33,10 @@ public class MarketService implements Initializable {
     private VBox container;
 
     private List<Service> serviceList;
+    private List<Service> serviceListDynamic;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-//        sortOrder.getToggleGroup().getToggles().addAll("1","0");
         serviceList = new ServiceService().getAll();
         updateContainer(serviceList);
         Field[] fields = Service.class.getDeclaredFields();
@@ -44,22 +44,23 @@ public class MarketService implements Initializable {
         sortMenu.getItems().addAll(Arrays.stream(fields).skip(1).filter(field -> !field.getName().equals("cree_le") && !field.getName().equals("mis_a_jour_le")).map(f-> f.getName().toUpperCase().replace('_', ' ')).toList());
         searchField.setOnKeyTyped(keyEvent -> {
             if (searchMenu.getValue() == null) return;
-            updateContainer(serviceList.stream().filter(s ->
-                switch (searchMenu.getValue()){
-                    case "FREELANCER" -> s.getFreelancer().getNom().toLowerCase().contains(searchField.getText().toLowerCase());
-                    case "TITRE" -> s.getTitre().toLowerCase().contains(searchField.getText().toLowerCase());
-                    case "DESCRIPTION" -> s.getDescription().toLowerCase().contains(searchField.getText().toLowerCase());
-                    case "EXPERTISE" -> s.getExpertise().toLowerCase().contains(searchField.getText().toLowerCase());
-                    case "DUREE JOURS" -> Integer.toString(s.getDuree_jours()).toLowerCase().contains(searchField.getText().toLowerCase());
-                    case "PRIX" -> s.getPrix().toString().toLowerCase().contains(searchField.getText().toLowerCase());
-                    case "MODE PAIEMENT" -> s.getMode_paiement().toString().toLowerCase().contains(searchField.getText().toLowerCase());
-                    default -> false;
-                }
-            ).toList());
+            serviceListDynamic = serviceList.stream().filter(s ->
+                    switch (searchMenu.getValue()){
+                        case "FREELANCER" -> s.getFreelancer().getNom().toLowerCase().contains(searchField.getText().toLowerCase());
+                        case "TITRE" -> s.getTitre().toLowerCase().contains(searchField.getText().toLowerCase());
+                        case "DESCRIPTION" -> s.getDescription().toLowerCase().contains(searchField.getText().toLowerCase());
+                        case "EXPERTISE" -> s.getExpertise().toLowerCase().contains(searchField.getText().toLowerCase());
+                        case "DUREE JOURS" -> Integer.toString(s.getDuree_jours()).toLowerCase().contains(searchField.getText().toLowerCase());
+                        case "PRIX" -> s.getPrix().toString().toLowerCase().contains(searchField.getText().toLowerCase());
+                        case "MODE PAIEMENT" -> s.getMode_paiement().toString().toLowerCase().contains(searchField.getText().toLowerCase());
+                        default -> false;
+                    }
+                ).toList();
+            updateContainer(serviceListDynamic);
         });
         sortMenu.valueProperty().addListener(keyEvent -> {
             if (sortMenu.getValue() == null) return;
-            updateContainer(serviceList.stream().sorted((i1, i2) ->
+            serviceListDynamic = serviceList.stream().sorted((i1, i2) ->
                     switch (sortMenu.getValue()){
                         case "FREELANCER" -> i1.getFreelancer().getNom().compareTo(i2.getFreelancer().getNom());
                         case "TITRE" -> i1.getTitre().compareTo(i2.getTitre());
@@ -70,7 +71,15 @@ public class MarketService implements Initializable {
                         case "MODE PAIEMENT" -> i1.getMode_paiement().compareTo(i2.getMode_paiement());
                         default -> 1;
                     }
-            ).toList());
+            ).toList();
+            updateContainer(serviceListDynamic);
+        });
+        sortOrder.selectedProperty().addListener(e -> {
+            if (serviceListDynamic == null) return;
+            serviceListDynamic = serviceListDynamic.stream()
+                    .sorted((s1, s2) -> serviceListDynamic.indexOf(s2) - serviceListDynamic.indexOf(s1))
+                    .toList();
+            updateContainer(serviceListDynamic);
         });
     }
 

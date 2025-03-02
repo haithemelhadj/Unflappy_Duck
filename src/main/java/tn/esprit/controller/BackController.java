0f184eae718eb94e31549d56bbf4a85fe8710161
hhakiri <101249657+hhakiri@users.noreply.github.com>
2025-03-02 23:22:03 +0100

    back office store

--- src/main/java/tn/esprit/controller/BackController.java
+++ src/main/java/tn/esprit/controller/BackController.java
@@ -1,9 +1,11 @@
 package tn.esprit.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -30,5 +32,13 @@ public class BackController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    public void show_article(ActionEvent actionEvent) throws IOException {
+        Router.borderPaneCenterInsert(storeBorderPane,"/BackOffice/GestionStore/ArticleBoutique.fxml");
+    }
+
+    public void show_panier(ActionEvent actionEvent) throws IOException {
+        Router.borderPaneCenterInsert(storeBorderPane,"/BackOffice/GestionStore/Panier.fxml");
     }
 }

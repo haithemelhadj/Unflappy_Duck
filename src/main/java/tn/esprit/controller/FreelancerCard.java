85b874b960b050ac8b429cbe0ac4b61274de5963
trt <seifbkh97@gmail.com>
2025-03-03 11:23:46 +0100

    fixed some issues in the ui setting

--- src/main/java/tn/esprit/controller/FreelancerCard.java
+++ src/main/java/tn/esprit/controller/FreelancerCard.java
@@ -3,9 +3,19 @@ package tn.esprit.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
 
 public class FreelancerCard {
+    @FXML
+    private Label userName;
+    @FXML
+    private Label statusLabel;
+    @FXML
+    private ImageView profilePic;
+    @FXML
+    private Circle statusCircle;
     @FXML
     private ProgressBar xpBar;
     @FXML
--- src/main/java/tn/esprit/controller/RecentJob.java
+++ src/main/java/tn/esprit/controller/RecentJob.java
@@ -1,4 +1,17 @@
 package tn.esprit.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class RecentJob {
+    @FXML
+    private Label durationAndPrice;
+    @FXML
+    private Label serviceTitle;
+    @FXML
+    private Label earnedXP;
+    @FXML
+    private Label rating;
+
+
 }

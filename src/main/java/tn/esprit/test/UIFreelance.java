7f0d82c68199d9bf6c36df50661ab5ada2785398
alaeddineturki <114579268+alaeddineturki@users.noreply.github.com>
2025-03-03 14:31:49 +0100

    i enhanced some part of ui

--- src/main/java/tn/esprit/test/UIFreelance.java
+++ src/main/java/tn/esprit/test/UIFreelance.java
@@ -19,7 +19,7 @@ public class UIFreelance extends Application {
     public void start(Stage primaryStage) throws SQLException {
         Router.initialize(primaryStage);
         Controller con = new Controller();
-       Session.start(new ServiceUtilisateur().getUtilisateurById(2));
+      // Session.start(new ServiceUtilisateur().getUtilisateurById(1));
         con.start();
     }
 }

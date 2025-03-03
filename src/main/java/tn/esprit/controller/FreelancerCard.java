dd5795041133bd7ee6efe99f12fd702be4e6e477
trt <seifbkh97@gmail.com>
2025-03-03 11:28:30 +0100

    logic assembled for freelancer card

--- src/main/java/tn/esprit/controller/FreelancerCard.java
+++ src/main/java/tn/esprit/controller/FreelancerCard.java
@@ -5,9 +5,14 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
 
+import java.util.List;
+
 public class FreelancerCard {
+    @FXML
+    private VBox recentJobsContainer;
     @FXML
     private Label userName;
     @FXML
@@ -23,11 +28,17 @@ public class FreelancerCard {
     @FXML
     private HBox skillsContainer;
 
+    private final int nbrRecentJobs = 3;
+
     private Label addSkill(String skill){
         Label l = new Label();
         l.setText(skill);
         l.setStyle("-fx-background-color: #f0f0f0; -fx-padding: 8px; -fx-background-radius: 15;");
         return l;
+    }
+
+    private List<HBox> recentJobs(){
+
     }
 
 }

1ff5dbb5c53fc14442f4a102885c9c0eac3c8d84
Elhaj Elhaithem <101217966+haithemelhadj@users.noreply.github.com>
2025-03-04 17:07:30 +0100

    gestion equipe  bg transparent

--- src/main/java/tn/esprit/test/MainFX.java
+++ src/main/java/tn/esprit/test/MainFX.java
@@ -31,8 +31,8 @@ public class MainFX extends Application {
     public void start(Stage primaryStage) {
 
         //FXMLLoader loader =new FXMLLoader(getClass().getResource("/FrontOffice/GestionEvenement/event_form.fxml"));
-        //FXMLLoader loader =new FXMLLoader(getClass().getResource("/FrontOffice/GestionEquipe/GestionEquipe.fxml"));
-        FXMLLoader loader =new FXMLLoader(getClass().getResource("/FrontOffice/GestionEquipe/GestionTache.fxml"));
+        FXMLLoader loader =new FXMLLoader(getClass().getResource("/FrontOffice/GestionEquipe/GestionEquipe.fxml"));
+        //FXMLLoader loader =new FXMLLoader(getClass().getResource("/FrontOffice/GestionEquipe/GestionTache.fxml"));
         try {
             Parent root = loader.load();
             Scene scene = new Scene(root);
